324ae19a07439be49f17c6095a8b9cc3a2cb9303
Vanshikav123 <vanshikav928@gmail.com>
2025-07-28 22:29:48 +0530

    discard command
    Signed-off-by: Vanshikav123 <vanshikav928@gmail.com>

--- app/main.go
+++ app/main.go
@@ -65,8 +65,115 @@ func main() {
 	}
 
 }
+
+// type to store a command to be queued
+
+type QueuedCommand struct {
+	Command string
+	Args    []string
+}
+
+func executeCommand(command string, args []string) []byte {
+
+	switch command {
+	case "PING":
+		return []byte("+PONG\r\n")
+	case "ECHO":
+		if len(args) < 1 {
+			return []byte("-ERR wrong number of arguments for 'echo' command\r\n")
+		}
+		response := fmt.Sprintf("$%d\r\n%s\r\n", len(args[0]), args[0])
+		return []byte(response)
+	case "SET":
+		if len(args) < 2 {
+			return []byte("-ERR wrong number of arguments for 'set' command\r\n")
+		}
+		key := args[0]
+		value := args[1]
+
+		var expiry time.Time
+		if len(args) > 3 && strings.ToUpper(args[2]) == "PX" {
+			expiryMillis, err := strconv.ParseInt(args[3], 10, 64)
+			if err != nil {
+				return []byte("-ERR value is not an integer or out of range\r\n")
+			}
+			expiry = time.Now().Add(time.Duration(expiryMillis) * time.Millisecond)
+		}
+
+		dataStore.Lock()
+		defer dataStore.Unlock()
+		dataStore.data[key] = valueEntry{
+			value:   value,
+			expires: expiry,
+		}
+		return []byte("+OK\r\n")
+
+	case "GET":
+		if len(args) < 1 {
+			return []byte("-ERR wrong number of arguments for 'get' command\r\n")
+		}
+		key := args[0]
+
+		dataStore.RLock() // Acquire a read lock first
+
+		entry, ok := dataStore.data[key]
+
+		//  Key doesn't exist at all
+		if !ok {
+			dataStore.RUnlock() // Release the read lock
+			return []byte("$-1\r\n")
+		}
+
+		//  Key exists, check if it's expired
+		if !entry.expires.IsZero() && time.Now().After(entry.expires) {
+			dataStore.RUnlock() // Release the read lock so we can get a write lock
+
+			// Now get a write lock to safely delete the key
+			dataStore.Lock()
+			entry, ok = dataStore.data[key]
+			if ok && !entry.expires.IsZero() && time.Now().After(entry.expires) {
+				delete(dataStore.data, key)
+			}
+			dataStore.Unlock() // Release the write lock
+			return []byte("$-1\r\n")
+		}
+
+		response := fmt.Sprintf("$%d\r\n%s\r\n", len(entry.value), entry.value)
+		dataStore.RUnlock() // Release the read lock
+		return []byte(response)
+
+	case "INCR":
+		if len(args) < 1 {
+			return []byte("-ERR wrong number of arguments for 'incr' command\r\n")
+		}
+		key := args[0]
+		dataStore.Lock()
+		defer dataStore.Unlock()
+		entry, ok := dataStore.data[key]
+		if !ok {
+			newValue := 1
+			dataStore.data[key] = valueEntry{value: strconv.Itoa(newValue)}
+			return []byte(fmt.Sprintf(":%d\r\n", newValue))
+		} else {
+			currentValue, err := strconv.Atoi(entry.value)
+			if err != nil {
+				return []byte("-ERR value is not an integer or out of range\r\n")
+			} else {
+				newValue := currentValue + 1
+				entry.value = strconv.Itoa(newValue)
+				dataStore.data[key] = entry
+				return []byte(fmt.Sprintf(":%d\r\n", newValue))
+			}
+		}
+	default:
+		return []byte(fmt.Sprintf("-ERR unknown command '%s'\r\n", command))
+	}
+}
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+
+	inTransactions := false
+	var commandQueue []QueuedCommand
 
 	reader := bufio.NewReader(conn)
 	for {
@@ -118,69 +225,21 @@ func handleConnection(conn net.Conn) {
 		}
 
 		command := strings.ToUpper(args[0])
+		cmdArgs := args[1:]
+		// If we are in a transaction, queue commands instead of executing them.
+		if inTransactions && command != "MULTI" && command != "EXEC" && command != "DISCARD" && command != "WATCH" {
+			cmd := QueuedCommand{
+				Command: command,
+				Args:    args[1:],
+			}
+			commandQueue = append(commandQueue, cmd)
+			conn.Write([]byte("+QUEUED\r\n"))
+			continue
+
+		}
 
 		switch command {
-		case "PING":
-			conn.Write([]byte("+PONG\r\n"))
-		case "ECHO":
-			if len(args) < 2 {
-				conn.Write([]byte("-ERR wrong number of arguments for 'echo' command\r\n"))
-				continue
-			}
-			response := fmt.Sprintf("$%d\r\n%s\r\n", len(args[1]), args[1])
-			conn.Write([]byte(response))
-		case "SET":
-			if len(args) < 3 {
-				conn.Write([]byte("-ERR wrong number of arguments for 'set' command\r\n"))
-				continue
-			}
-			key := args[1]
-			value := args[2]
 
-			var expiry time.Time
-			if len(args) > 4 && strings.ToUpper(args[3]) == "PX" {
-				expiryMillis, err := strconv.ParseInt(args[4], 10, 64)
-				if err != nil {
-					conn.Write([]byte("-ERR value is not an integer or out of range\r\n"))
-					continue
-				}
-				expiry = time.Now().Add(time.Duration(expiryMillis) * time.Millisecond)
-			}
-
-			dataStore.Lock()
-			dataStore.data[key] = valueEntry{
-				value:   value,
-				expires: expiry,
-			}
-			dataStore.Unlock()
-			conn.Write([]byte("+OK\r\n"))
-		case "GET":
-			if len(args) < 2 {
-				conn.Write([]byte("-ERR wrong number of arguments for 'get' command\r\n"))
-				continue
-			}
-			key := args[1]
-
-			dataStore.RLock()
-
-			entry, ok := dataStore.data[key]
-
-			if !ok {
-				conn.Write([]byte("$-1\r\n"))
-			} else if !entry.expires.IsZero() && time.Now().After(entry.expires) {
-
-				dataStore.RUnlock() // Release read lock
-				dataStore.Lock()    // Get write lock to delete
-				delete(dataStore.data, key)
-				dataStore.Unlock() // Release write lock
-				conn.Write([]byte("$-1\r\n"))
-				continue
-			} else {
-				response := fmt.Sprintf("$%d\r\n%s\r\n", len(entry.value), entry.value)
-				conn.Write([]byte(response))
-			}
-
-			dataStore.RUnlock()
 		case "CONFIG":
 			if len(args) < 3 || strings.ToUpper(args[1]) != "GET" {
 				conn.Write([]byte("-ERR Syntax error for CONFIG command\r\n"))
@@ -219,34 +278,44 @@ func handleConnection(conn net.Conn) {
 				response += fmt.Sprintf("$%d\r\n%s\r\n", len(k), k)
 			}
 			conn.Write([]byte(response))
-		case "INCR":
-			if len(args) < 2 {
-				conn.Write([]byte("-ERR wrong number of arguments for 'incr' command\r\n"))
-				continue
-			}
-			key := args[1]
-			dataStore.Lock()
-			defer dataStore.Unlock()
 
-			entry, ok := dataStore.data[key]
-			if !ok {
-				newValue := 1
-				dataStore.data[key] = valueEntry{value: strconv.Itoa(newValue)}
-				conn.Write([]byte(fmt.Sprintf(":%d\r\n", newValue)))
+		case "MULTI":
+			inTransactions = true
+			conn.Write([]byte("+OK\r\n"))
+		case "EXEC":
+			if !inTransactions {
+				conn.Write([]byte("-ERR EXEC without MULTI\r\n"))
 			} else {
-				currentValue, err := strconv.Atoi(entry.value)
-				if err != nil {
-					conn.Write([]byte("-ERR value is not an integer or out of range\r\n"))
-				} else {
-					newValue := currentValue + 1
-					entry.value = strconv.Itoa(newValue)
-					dataStore.data[key] = entry
-					conn.Write([]byte(fmt.Sprintf(":%d\r\n", newValue)))
-				}
-			}
 
+				responses := make([][]byte, len(commandQueue))
+
+				for i, cmd := range commandQueue {
+					responses[i] = executeCommand(cmd.Command, cmd.Args)
+				}
+
+				var finalResponse strings.Builder
+				finalResponse.WriteString(fmt.Sprintf("*%d\r\n", len(responses)))
+				for _, res := range responses {
+					finalResponse.Write(res)
+				}
+
+				conn.Write([]byte(finalResponse.String()))
+
+				inTransactions = false
+				commandQueue = nil
+			}
+		case "DISCARD":
+			if !inTransactions {
+				conn.Write([]byte("-ERR DISCARD without MULTI\r\n"))
+			} else {
+				//reset the transaction state and clear the queue
+				inTransactions = false
+				commandQueue = nil
+				conn.Write([]byte("+OK\r\n"))
+			}
 		default:
-			conn.Write([]byte(fmt.Sprintf("-ERR unknown command '%s'\r\n", command)))
+			response := executeCommand(command, cmdArgs)
+			conn.Write(response)
 
 		}
 	}
